aa2f5760638e40f9f94d6a276159829e9984a662
huxichun <huxichun163@163.com>
2025-05-15 14:38:53 +0800

    refactor(locale-select): 优化语言选择组件，简化状态管理并提升可读性

--- src/components/common/locale-select.tsx
+++ src/components/common/locale-select.tsx
@@ -1,59 +1,51 @@
 'use client'
 
-import { SelectContent, SelectItem } from '@/components/ui/select'
-import { localesConfig, usePathname, useRouter } from '@/lib/i18n'
-import { reportEvent } from '@/lib/report'
+import { Select, SelectContent, SelectItem } from '@/components/ui/select'
+import { localesConfig, useRouter } from '@/lib/i18n'
 import { cn } from '@/lib/utils'
 import * as SelectPrimitive from '@radix-ui/react-select'
 import { ChevronDown, Globe } from 'lucide-react'
 import { useLocale, useTranslations } from 'next-intl'
-import { useSearchParams } from 'next/navigation'
 
 export function LocaleSelect({
   className,
 }: { className?: string }) {
   const t = useTranslations()
-  const currLocale = useLocale()
+  const router = useRouter()
+  const locale = useLocale()
 
   const items = localesConfig.map(item => (
     <SelectItem
       key={item.code}
       value={item.code}
-      onClick={() => {
-        reportEvent('click_top_language')
-      }}
     >
       {item.desc}
     </SelectItem>
   ))
 
-  const router = useRouter()
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
-
   const setLocale = (locale: string) => {
-    const path = `${pathname}?${searchParams.toString()}`
+    const path = `${location.pathname}${location.search}`
     router.replace(path, { locale })
   }
 
   return (
-    <SelectPrimitive.Root value={currLocale} onValueChange={setLocale}>
+    <Select value={locale} onValueChange={setLocale}>
       <SelectPrimitive.Trigger
-        aria-label={t('common.switch', { name: t('dict.language') })}
         className={cn(
           'inline-flex items-center gap-2 text-sm md:text-base',
           className,
         )}
+        aria-label={t('common.switch', { name: t('dict.language') })}
       >
-        <Globe size={24} />
-        <div className="max-md:hidden md:contents">
+        <Globe className="size-6" />
+        <div className="hidden md:contents">
           <SelectPrimitive.Value />
           <ChevronDown size={16} />
         </div>
       </SelectPrimitive.Trigger>
-      <SelectContent className="z-[1000] rounded-2xl bg-card">
+      <SelectContent align="center">
         {items}
       </SelectContent>
-    </SelectPrimitive.Root>
+    </Select>
   )
 }
